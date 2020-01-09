2aa1c1441bf3ef78595c78ea3ae8b56da71234e2
Sam Howe <showe@gwcu.gwcu.org>
2020-01-09 09:08:04 -0700

    First commit

---
+++ ADBulkUserAddTest.v4.ps1
@@ -0,0 +1,101 @@
+﻿Import-module activedirectory
+$Users=Import-csv "C:\UserImport\BulkUserImport.TEST.csv"
+$a=1;
+$b=1;
+$failedUsers = @()
+$usersAlreadyExist =@()
+$successUsers = @()
+$VerbosePreference = "Continue"
+$LogFolder = "C:\UserImport\log"
+
+
+ForEach($User in $Users)
+{
+   $FirstName = $User.FirstName.substring(0,1).toupper()+$User.FirstName.substring(1).tolower()
+   $LastName  = $User.LastName.substring(0,1).toupper()+$User.LastName.substring(1).tolower()
+   $FullName = $User.FirstName + " " + $User.LastName
+        for ($i = 1; $i -le $user.FirstName.Length; $i++) {
+                $SAM = $user.FirstName.Substring(0,$i) + $user.LastName #example John snow will be Jsnow
+                if (-not (get-aduser -Filter " samaccountname -eq '$SAM' ")) {
+                    "$SAM is available!"
+                    $SAM = $sam.ToLower()
+                    break
+                } else {
+                    if ($i -eq $user.firstname.Length) {
+                        "Username cannot be auto-generated"
+                        exit
+                    }
+                }
+            }
+            write-host "$SAM"
+
+   $dnsroot = '@' + (Get-ADDomain).dnsroot
+   $SAM=$sam.tolower()
+
+
+$Password = (convertto-securestring $User.Password -AsPlainText -Force)
+$UPN = $SAM + "$dnsroot" # change "$dnsroot to custom domain if you want, by default it will take from DNS ROOT"
+$OU="OU=Unsorted,OU=All Users,DC=gwcu,DC=gwcu,DC=org"
+$FileserverUser= "\\fileserver\users"
+$homeDirectory="$FileserverUser\$SAM"
+$LogonScript = "default.bat"
+$phone = "801-337-8300"
+
+
+try {
+    if (!(get-aduser -Filter {samaccountname -eq "$SAM"}))
+    {
+    $Parameters = @{
+    'SamAccountName'             = $Sam
+    'UserPrincipalName'          = $UPN
+    'Name'                       = $Fullname
+    'GivenName'                  = $User.FirstName
+    'Surname'                    = $User.Lastname
+    'AccountPassword'            = $Password
+    'ChangePasswordAtLogon'      = $true
+    'Enabled'                    = $true
+    'Path'                       = $OU
+    'Description'                = $User.jobtitle
+    'Office'                     = $User.Office
+    'homeDirectory'              = $homeDirectory
+    'homeDrive'                  = "U:"
+    'scriptpath'                 = $LogonScript
+    'DisplayName'                = $FullName
+    'officephone'                = $phone
+    #Attributes listed above that are imported from .csv need to have a $User. in front of the attribute. 
+    }
+
+New-ADUser @Parameters
+     Write-Verbose "[PASS] Created $FullName under $ou"
+     $successUsers += $FullName + "," +$SAM
+    }
+   
+}
+catch {
+    Write-Warning "[ERROR]Can't create user [$($FullName)] : $_"
+    $failedUsers += $FullName + "," +$SAM + "," +$_
+}
+
+#Add New User to Group
+Add-AdGroupMember -Identity Everybody $SAM
+
+#Add New User Home Folder (Had to create separate scripts since running code within produced errors.)
+$PSScriptRoot
+& "$PSScriptRoot\AddHomeFolder.ps1"
+}
+if ( !(test-path $LogFolder)) 
+    {
+    Write-Verbose "Folder [$($LogFolder)] does not exist, creating"
+    new-item $LogFolder -type directory -Force 
+    }
+
+Write-verbose "Writing logs"
+$failedUsers |ForEach-Object {"$($b).) $($_)"; $b++} | out-file -FilePath  $LogFolder\FailedUsers.log -Force -Verbose
+$successUsers | ForEach-Object {"$($a).) $($_)"; $a++} |out-file -FilePath  $LogFolder\successUsers.log -Force -Verbose
+
+$su=(Get-Content "$LogFolder\successUsers.log").count
+$fu=(Get-Content "$LogFolder\FailedUsers.log").count
+
+
+Write-Host "$fu Users Creation Failed and " -NoNewline -ForegroundColor red
+Write-Host "$su Users Successfully Created "  -NoNewline -ForegroundColor green
---
+++ AddHomeFolder.ps1
@@ -0,0 +1,34 @@
+﻿$Users=Import-csv "C:\UserImport\BulkUserImport.TEST.csv"
+$a=1;
+$b=1;
+$failedUsers = @()
+$usersAlreadyExist =@()
+$successUsers = @()
+$VerbosePreference = "Continue"
+$LogFolder = "C:\UserImport\log"
+
+
+ForEach($User in $Users)
+{
+$sam = $User.username
+$FileserverUser= "\\fileserver\users"
+$homeDirectory="$FileserverUser\$SAM"
+
+#Create HomeFolder
+    if (!(Test-Path "$homeDirectory"))
+
+        {
+            $NewFolder = New-Item -Path $FileserverUser -Name $sam -ItemType "Directory"
+            $Rights = [System.Security.AccessControl.FileSystemRights]"FullControl,Modify,ReadAndExecute,ListDirectory,Read,Write"
+            $InheritanceFlag = @([System.Security.AccessControl.InheritanceFlags]::ContainerInherit,[System.Security.AccessControl.InheritanceFlags]::ObjectInherit)
+            $PropagationFlag = [System.Security.AccessControl.PropagationFlags]::None
+            $objType =[System.Security.AccessControl.AccessControlType]::Allow
+            $dnsroot = (Get-ADDomain).dnsroot
+            $objUser = New-Object System.Security.Principal.NTAccount "$dnsroot\$sam"
+            $objACE = New-Object System.Security.AccessControl.FileSystemAccessRule `
+                    ($objUser, $Rights, $InheritanceFlag, $PropagationFlag, $objType)
+            $ACL = Get-Acl -Path $NewFolder
+            $ACL.AddAccessRule($objACE)
+            Set-ACL -Path $NewFolder.FullName -AclObject $ACL
+        }
+}
